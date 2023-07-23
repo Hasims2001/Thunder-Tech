f3062db9037c41ac5d84d7d0855ee6dc6c3a7f02
Hasim <hasims2001@gmail.com>
2023-07-23 22:28:48 +0530

    Fixed

--- src/AllRouters.jsx
+++ src/AllRouters.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import  {ProductList}  from "./Ranvijay/ProductList";
+import { ProductList } from "./Ranvijay/ProductList";
 import SingleProductpage from "./Ranvijay/SingleProductpage";
 
 function AllRouters() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>}></Route>
-      <Route path='/products' element={<ProductList/>} />
-
-        <Route path="/products/:id" element={<SingleProductpage/>}/>
+      <Route path="/products" element={<ProductList />} />
+      <Route path="/products/:id" element={<SingleProductpage />} />
     </Routes>
   );
 }
